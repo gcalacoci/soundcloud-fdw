ca9036a2393a003fb69dc6c1686904027a035764
Giulio Calacoci <giulio.calacoci@2ndquadrant.com>
2016-04-12 05:05:48 +0200

    license added
    Signed-off-by: Giulio Calacoci <giulio.calacoci@2ndquadrant.com>

--- soundcloud_fdw/SoundcloudFDW.py
+++ soundcloud_fdw/SoundcloudFDW.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2016 Giulio Calacoci
+#
+# This file is part of SoundcloudFDW.
+#
+# SoundcloudFDW is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SoundcloudFDW is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SoundcloudFDW.  If not, see <http://www.gnu.org/licenses/>.
+
 from logging import ERROR, DEBUG
 
 import soundcloud
@@ -25,11 +42,10 @@ class SoundcloudForeignDataWrapper(ForeignDataWrapper):
         log_to_postgres('Soundcloud FDW Config options:  %s' % options, DEBUG)
         log_to_postgres('Soundcloud FDW Config columns:  %s' % columns, DEBUG)
 
-        if options.has_key('apikey'):
-            self.apikey = options['apikey']
-        else:
+        if 'apikey' not in options:
             log_to_postgres('Option apikey is required for '
                             'the Soundcloud FDW setup.', ERROR)
+        self.apikey = options['apikey']
 
     def execute(self, quals, columns, **kwargs):
         """
